3292e92a5f00eaf49d7c1719dbc81a3e143accee
picalabsuperadmin <superadmin@picalab.ai>
2024-09-14 13:07:08 +0800

    Update index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,13 +1,9 @@
-import { NATIVE_MINT } from '@solana/spl-token'
-
-export * from './GambaProvider'
-export * from './constants'
-export * from './decoders'
-export * from './events'
-export { IDL, type Gamba as GambaIdl } from './idl'
-export * from './instructions'
-export * from './pdas'
-export * from './types'
-export * from './utils'
-export { NATIVE_MINT }
-
+export * from './WinbaProvider';
+export * from './constants';
+export * from './decoders';
+export * from './events';
+export * from './idl';
+export * from './instructions';
+export * from './pdas';
+export * from './types';
+export * from './utils';
