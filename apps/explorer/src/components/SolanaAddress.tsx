cee8aee5ecfdf9ac16bdb4c19465043f205ac7c4
picalabsuperadmin <superadmin@picalab.ai>
2024-09-14 16:17:33 +0800

    Update SolanaAddress.tsx

--- apps/explorer/src/components/SolanaAddress.tsx
+++ apps/explorer/src/components/SolanaAddress.tsx
@@ -1,26 +1,26 @@
-import { useToast } from "@/hooks"
-import { ClipboardIcon } from "@radix-ui/react-icons"
-import { Flex, IconButton, Link } from "@radix-ui/themes"
-import { PublicKey } from "@solana/web3.js"
-import React from "react"
+import { useToast } from "@/hooks";
+import { ClipboardIcon } from "@radix-ui/react-icons";
+import { Flex, IconButton, Link } from "@radix-ui/themes";
+import React from "react";
 
 interface Props {
-  address: PublicKey | string
-  truncate?: boolean
-  plain?: boolean
+  address: string;
+  truncate?: boolean;
+  plain?: boolean;
 }
 
-export function SolanaAddress(props: Props) {
-  const toast = useToast()
+export function OptimismAddress(props: Props) {
+  const toast = useToast();
 
   const copy = () => {
-    window.navigator.clipboard.writeText(props.address.toString())
-    toast({ title: "Copied", description: "Copied to clipboard" })
-  }
+    window.navigator.clipboard.writeText(props.address);
+    toast({ title: "Copied", description: "Copied to clipboard" });
+  };
 
-  const address = props.address.toString()
-
-  const text = props.truncate ? address.slice(0, 6) + "..." + address.slice(-6) : address
+  const address = props.address;
+  const text = props.truncate
+    ? address.slice(0, 6) + "..." + address.slice(-6)
+    : address;
 
   return (
     <Flex align="center" gap="2">
@@ -29,7 +29,11 @@ export function SolanaAddress(props: Props) {
           {text}
         </>
       ) : (
-        <Link target="_blank" href={`https://solscan.io/address/${props.address.toString()}`} rel="noreferrer">
+        <Link
+          target="_blank"
+          href={`https://optimistic.etherscan.io/address/${props.address}`}
+          rel="noreferrer"
+        >
           {text}
         </Link>
       )}
@@ -37,5 +41,5 @@ export function SolanaAddress(props: Props) {
         <ClipboardIcon />
       </IconButton>
     </Flex>
-  )
+  );
 }
