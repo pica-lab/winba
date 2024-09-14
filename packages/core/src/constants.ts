2639035f5454fb30bc8af40d2fd55d51ca4ec481
picalabsuperadmin <superadmin@picalab.ai>
2024-09-14 12:55:04 +0800

    Update constants.ts

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,16 +1,16 @@
-import { PublicKey } from '@solana/web3.js'
+import { ethers } from 'ethers';
 
-export const PROGRAM_ID = new PublicKey('Gamba2hK6KV3quKq854B3sQG1WMdq3zgQLPKqyK4qS18')
-export const SYSTEM_PROGRAM = new PublicKey('11111111111111111111111111111111')
+// Load contract address from environment variables
+export const PROGRAM_ID = process.env.CONTRACT_ADDRESS || ''; 
+export const SYSTEM_PROGRAM = ethers.constants.AddressZero;
+export const BPS_PER_WHOLE = 10000;
 
-export const BPS_PER_WHOLE = 10_000
-
-export const GAME_SEED = 'GAME'
-export const PLAYER_SEED = 'PLAYER'
-export const POOL_SEED = 'POOL'
-export const GAMBA_STATE_SEED = 'GAMBA_STATE'
-export const POOL_ATA_SEED = 'POOL_ATA'
-export const POOL_JACKPOT_SEED = 'POOL_JACKPOT'
-export const POOL_BONUS_UNDERLYING_TA_SEED = 'POOL_BONUS_UNDERLYING_TA'
-export const POOL_BONUS_MINT_SEED = 'POOL_BONUS_MINT'
-export const POOL_LP_MINT_SEED = 'POOL_LP_MINT'
+export const GAME_SEED = 'GAME';
+export const PLAYER_SEED = 'PLAYER';
+export const POOL_SEED = 'POOL';
+export const GAMBA_STATE_SEED = 'GAMBA_STATE';
+export const POOL_ATA_SEED = 'POOL_ATA';
+export const POOL_JACKPOT_SEED = 'POOL_JACKPOT';
+export const POOL_BONUS_UNDERLYING_TA_SEED = 'POOL_BONUS_UNDERLYING_TA';
+export const POOL_BONUS_MINT_SEED = 'POOL_BONUS_MINT';
+export const POOL_LP_MINT_SEED = 'POOL_LP_MINT';
