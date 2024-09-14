938304d374e107487ce49e8cd8c8c88feb8aed69
picalabsuperadmin <superadmin@picalab.ai>
2024-09-14 15:56:26 +0800

    Update price.ts

--- apps/api/src/price.ts
+++ apps/api/src/price.ts
@@ -1,6 +1,6 @@
 import { config } from './config'
 
-const priceData: Record<string, {lastFetch: number, usdPerUnit: number}> = {}
+const priceData: Record<string, { lastFetch: number, usdPerUnit: number }> = {}
 
 export const getPrices = async (
   tokens: string[],
@@ -17,35 +17,28 @@ export const getPrices = async (
 
   console.log('Fetching token prices', tokensToFetch)
 
-  const response = await fetch(`https://mainnet.helius-rpc.com/?api-key=${config().HELIUS_API_KEY}`, {
+  const response = await fetch(`https://api.coingecko.com/api/v3/simple/token_price/ethereum`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      jsonrpc: '2.0',
-      id: 'my-id',
-      method: 'getAssetBatch',
-      params: { ids: tokensToFetch },
+      ids: tokensToFetch.join(','),
+      vs_currencies: 'usd',
     }),
   })
 
-  const { result } = (await response.json()) as { result: any[] }
+  const result = await response.json()
 
-  const tokenPrices = result.reduce(
-    (prev, token) => {
-      const tokenInfo = token.token_info
-      const pricePerToken = tokenInfo?.price_info?.price_per_token ?? 0
-      const decimals = token.token_info?.decimals ?? 9
-      return {
-        ...prev,
-        [token.id]: pricePerToken / (10 ** decimals),
-      }
-    },
-    {} as Record<string, number>,
-  )
+  const tokenPrices = tokensToFetch.reduce((prev, token) => {
+    const price = result[token]?.usd ?? 0
+    return {
+      ...prev,
+      [token]: price,
+    }
+  }, {} as Record<string, number>)
 
   for (const key of tokensToFetch) {
     if (tokenPrices[key] === undefined) {
-      console.log('‼️ No price data for', key)
+      console.log('‼ No price data for', key)
     }
 
     priceData[key] = {
