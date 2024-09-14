c1d0a3abd869da9e7a7b3fb15c77868eb9e50579
picalabsuperadmin <superadmin@picalab.ai>
2024-09-14 15:54:27 +0800

    Update config.ts

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -6,16 +6,18 @@ export const config = () => {
   if (configed) {
     return process.env
   }
-
+  
   dotenvConfig()
 
-  if (!process.env.HELIUS_API_KEY) {
-    throw new Error('Helius key not specified')
+  if (!process.env.OP_RPC_URL) {
+    throw new Error('Optimism RPC URL not specified')
   }
 
-  if (!process.env.SOLANA_RPC_ENDPOINT) {
-    throw new Error('RPC not specified')
+  if (!process.env.THIRDWEB_CLIENT_ID || !process.env.THIRDWEB_SECRET_KEY) {
+    throw new Error('Thirdweb credentials not specified')
   }
+
   configed = true
   return process.env
 }
+```
