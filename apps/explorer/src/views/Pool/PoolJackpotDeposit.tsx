d4de96adc89886bd8f352bc8538c14377cb880f9
picalabsuperadmin <superadmin@picalab.ai>
2024-09-14 16:56:41 +0800

    Update PoolJackpotDeposit.tsx

--- apps/explorer/src/views/Pool/PoolJackpotDeposit.tsx
+++ apps/explorer/src/views/Pool/PoolJackpotDeposit.tsx
@@ -1,92 +1,76 @@
-import { Button, Dialog, Flex, Grid, Heading, IconButton, Text, TextField } from "@radix-ui/themes"
-import { createTransferInstruction, getAssociatedTokenAddressSync } from "@solana/spl-token"
-import { decodeAta, getPoolJackpotTokenAccountAddress, getUserWsolAccount, isNativeMint, wrapSol } from "gamba-core-v2"
-import { useAccount, useSendTransaction, useWalletAddress } from "gamba-react-v2"
-import React from "react"
-
-import { UiPool } from "@/views/Dashboard/PoolList"
-import { TokenValue2 } from "@/components/TokenValue2"
-import { useBalance, useToast } from "@/hooks"
-import { useTokenMeta } from "@/hooks/useTokenMeta"
+import { Button, Dialog, Flex, Grid, Heading, IconButton, Text, TextField } from "@radix-ui/themes";
+import { createTransferInstruction, getAssociatedTokenAddressSync } from "@solana/spl-token";
+import { decodeAta, getPoolJackpotTokenAccountAddress, isNativeMint } from "winba-core";
+import { useAccount, useSendTransaction, useWalletAddress } from "winba-react";
+import React from "react";
+import { UiPool } from "@/views/Dashboard/PoolList";
+import { TokenValue2 } from "@/components/TokenValue2";
+import { useBalance, useToast } from "@/hooks";
+import { useTokenMeta } from "@/hooks/useTokenMeta";
 
 export function PoolJackpotDeposit({ pool }: { pool: UiPool }) {
-  const [donateAmountText, setDonateAmountText] = React.useState("")
-  const sendTransaction = useSendTransaction()
-  const user = useWalletAddress()
-  const toast = useToast()
-  const token = useTokenMeta(pool.underlyingTokenMint)
-  const balances = useBalance(pool.underlyingTokenMint)
-  const wSolAccount = useAccount(getUserWsolAccount(user), decodeAta)
-
-  const amount = Math.round(Number(donateAmountText) * (10 ** token.decimals))
+  const [donateAmountText, setDonateAmountText] = React.useState("");
+  const sendTransaction = useSendTransaction();
+  const user = useWalletAddress();
+  const toast = useToast();
+  const token = useTokenMeta(pool.underlyingTokenMint);
+  const balances = useBalance(pool.underlyingTokenMint);
+  const amount = Math.round(Number(donateAmountText) * 10 ** token.decimals);
 
   const donateToJackpot = async () => {
-    const { publicKey, state } = pool
-    const underlyingTokenMint = state.underlyingTokenMint
-    const userAta = getAssociatedTokenAddressSync(underlyingTokenMint, user)
-    const poolJackpotAddress = getPoolJackpotTokenAccountAddress(publicKey)
+    const { publicKey, state } = pool;
+    const underlyingTokenMint = state.underlyingTokenMint;
+    const userAta = getAssociatedTokenAddressSync(underlyingTokenMint, user);
+    const poolJackpotAddress = getPoolJackpotTokenAccountAddress(publicKey);
+    const transferInstruction = createTransferInstruction(userAta, poolJackpotAddress, user, amount);
 
-    const transferInstruction = createTransferInstruction(
-      userAta,
-      poolJackpotAddress,
-      user,
-      amount,
-    )
+    const instructions = isNativeMint(underlyingTokenMint)
+      ? [transferInstruction]
+      : [transferInstruction];
 
-    const instructions = isNativeMint(underlyingTokenMint) ? [...(await wrapSol(user, amount, !wSolAccount)), transferInstruction] : [transferInstruction]
-
-    await sendTransaction(instructions, { confirmation: "confirmed" })
+    await sendTransaction(instructions, { confirmation: "confirmed" });
 
     toast({
       title: "Donation Successful",
       description: "You have successfully donated to the jackpot.",
-    })
-  }
+    });
+  };
 
   return (
     <Grid gap="2">
-      <Heading>
-        Fund Jackpot
-      </Heading>
-      <Text color="gray">
-        Donate {token.symbol} to this pool's jackpot.
-      </Text>
+      <Heading>Fund Jackpot</Heading>
+      <Text color="gray">Donate {token.symbol} to this pool's jackpot.</Text>
       <TextField.Root>
         <TextField.Input
           placeholder="Amount"
           value={donateAmountText}
           size="3"
-          onFocus={e => e.target.select()}
-          onChange={event => setDonateAmountText(event.target.value)}
+          onFocus={(e) => e.target.select()}
+          onChange={(event) => setDonateAmountText(event.target.value)}
         />
         <TextField.Slot>
-          <IconButton onClick={() => setDonateAmountText(String(balances.balance / (10 ** token.decimals)))} size="1" variant="ghost">
+          <IconButton
+            onClick={() => setDonateAmountText(String(balances.balance / 10 ** token.decimals))}
+            size="1"
+            variant="ghost"
+          >
             MAX
           </IconButton>
         </TextField.Slot>
       </TextField.Root>
       <Flex justify="between">
-        <Text size="2" color="gray">
-          Balance
-        </Text>
+        <Text size="2" color="gray">Balance</Text>
         <Text size="2">
           <TokenValue2 exact amount={balances.balance} mint={pool.underlyingTokenMint} />
         </Text>
       </Flex>
       <Dialog.Root>
         <Dialog.Trigger>
-          <Button size="3" variant="soft">
-            Donate
-          </Button>
+          <Button size="3" variant="soft">Donate</Button>
         </Dialog.Trigger>
         <Dialog.Content>
           <Grid gap="2">
-            <Text>
-              Donating to the Jackpot only serves to further incentivise players to play in the pool. You get nothing in return.
-            </Text>
-            <Text>
-              It is not possible to withdraw the money.
-            </Text>
+            <Text>Donating to the Jackpot only serves to incentivize players. It is not possible to withdraw the money.</Text>
             <Dialog.Close>
               <Button size="3" color="red" variant="soft" onClick={donateToJackpot}>
                 Donate <TokenValue2 exact amount={amount} mint={pool.underlyingTokenMint} />
@@ -96,5 +80,5 @@ export function PoolJackpotDeposit({ pool }: { pool: UiPool }) {
         </Dialog.Content>
       </Dialog.Root>
     </Grid>
-  )
+  );
 }
