fa85744f122ad811f7ed415794b39d5c0a94b686
picalabsuperadmin <superadmin@picalab.ai>
2024-09-14 16:57:51 +0800

    Update PoolMintBonus.tsx

--- apps/explorer/src/views/Pool/PoolMintBonus.tsx
+++ apps/explorer/src/views/Pool/PoolMintBonus.tsx
@@ -1,69 +1,51 @@
-import { Button, Flex, Grid, Heading, IconButton, Text, TextField } from "@radix-ui/themes"
-import { decodeAta, getUserWsolAccount, isNativeMint, wrapSol } from "gamba-core-v2"
-import { useAccount, useGambaProvider, useSendTransaction, useWalletAddress } from "gamba-react-v2"
-import React from "react"
-import { mutate } from "swr"
-
-import { TokenValue2 } from "@/components/TokenValue2"
-import { useBalance, useToast } from "@/hooks"
-import { useTokenMeta } from "@/hooks/useTokenMeta"
-import { UiPool } from "@/views/Dashboard/PoolList"
-import { stringtoBigIntUnits } from "./PoolDeposit"
+import { Button, Flex, Grid, Heading, IconButton, Text, TextField } from "@radix-ui/themes";
+import { useAccount, useWinbaProvider, useSendTransaction, useWalletAddress } from "winba-react";
+import React from "react";
+import { mutate } from "swr";
+import { TokenValue2 } from "@/components/TokenValue2";
+import { useBalance, useToast } from "@/hooks";
+import { useTokenMeta } from "@/hooks/useTokenMeta";
+import { UiPool } from "@/views/Dashboard/PoolList";
+import { stringToBigIntUnits } from "./PoolDeposit";
+import { USDC_ADDRESS } from "@/constants";
 
 export function PoolMintBonus({ pool }: { pool: UiPool }) {
-  const gamba = useGambaProvider()
-  const [amountText, setAmountText] = React.useState("")
-  const sendTransaction = useSendTransaction()
-  const user = useWalletAddress()
-  const toast = useToast()
-  const token = useTokenMeta(pool.underlyingTokenMint)
-  const balances = useBalance(pool.underlyingTokenMint)
-  const wSolAccount = useAccount(getUserWsolAccount(user), decodeAta)
-
+  const winba = useWinbaProvider();
+  const [amountText, setAmountText] = React.useState("");
+  const sendTransaction = useSendTransaction();
+  const user = useWalletAddress();
+  const toast = useToast();
+  const token = useTokenMeta(pool.underlyingTokenMint);
+  const balances = useBalance(pool.underlyingTokenMint);
   const mintBonusTokens = async () => {
     try {
-      const amount = stringtoBigIntUnits(amountText, token.decimals)
+      const amount = stringToBigIntUnits(amountText, token.decimals);
+      const { publicKey, state } = pool;
+      const underlyingTokenMint = state.underlyingTokenMint;
 
-      const { publicKey, state } = pool
-      const underlyingTokenMint = state.underlyingTokenMint
-
-      // const poolBonusMint = getPoolBonusAddress(publicKey)
-
-      const mintInstructions = gamba.mintBonusTokens(
+      const mintInstructions = winba.mintBonusTokens(
         publicKey,
-        underlyingTokenMint,
+        underlyingTokenMint === USDC_ADDRESS ? USDC_ADDRESS : underlyingTokenMint,
         amount,
-      )
+      );
 
-      if (isNativeMint(underlyingTokenMint)) {
-        const wrapSolInstructions = await wrapSol(
-          user,
-          amount,
-          !wSolAccount,
-        )
-        return sendTransaction([...wrapSolInstructions, mintInstructions], { confirmation: "confirmed" })
-      }
+      await sendTransaction(mintInstructions, { confirmation: "confirmed" });
 
-      await sendTransaction(mintInstructions, { confirmation: "confirmed" })
-
-      mutate(`pool-${publicKey.toBase58()}`)
-
-      toast({ title: "Bonus Tokens Minted", description: "" })
+      mutate(`pool-${publicKey.toBase58()}`);
+      toast({ title: "Bonus Tokens Minted", description: "" });
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   return (
     <Grid gap="2">
-      <Heading>
-        Mint bonus tokens
-      </Heading>
+      <Heading>Mint bonus tokens</Heading>
       <Text color="gray">
         Bonus tokens can be used as free plays.
         <ul>
           <li>The tokens can only be used by playing in this specific pool.</li>
-          <li>The tokens can not be swapped back to {token.symbol}.</li>
+          <li>The tokens cannot be swapped back to {token.symbol}.</li>
         </ul>
       </Text>
       <TextField.Root>
@@ -80,16 +62,12 @@ export function PoolMintBonus({ pool }: { pool: UiPool }) {
         </TextField.Slot>
       </TextField.Root>
       <Flex justify="between">
-        <Text size="2" color="gray">
-          Balance
-        </Text>
+        <Text size="2" color="gray">Balance</Text>
         <Text size="2">
           <TokenValue2 exact amount={balances.balance} mint={pool.underlyingTokenMint} />
         </Text>
       </Flex>
-      <Button size="3" variant="soft" onClick={mintBonusTokens}>
-        Mint
-      </Button>
+      <Button size="3" variant="soft" onClick={mintBonusTokens}>Mint</Button>
     </Grid>
-  )
+  );
 }
