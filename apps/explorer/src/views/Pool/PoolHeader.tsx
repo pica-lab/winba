cbf7e7754e44b566aba1d469f8d869a03182cc90
picalabsuperadmin <superadmin@picalab.ai>
2024-09-14 16:56:06 +0800

    Update PoolHeader.tsx

--- apps/explorer/src/views/Pool/PoolHeader.tsx
+++ apps/explorer/src/views/Pool/PoolHeader.tsx
@@ -1,41 +1,33 @@
-import { GearIcon, InfoCircledIcon } from "@radix-ui/react-icons"
-import { Dialog, Flex, Heading, IconButton, Text } from "@radix-ui/themes"
-import { decodeGambaState, getGambaStateAddress, getPoolBonusAddress, getPoolLpAddress } from "gamba-core-v2"
-import { useAccount, useWalletAddress } from "gamba-react-v2"
-import React from "react"
-import { NavLink, useNavigate } from "react-router-dom"
+import { GearIcon, InfoCircledIcon } from "@radix-ui/react-icons";
+import { Dialog, Flex, Heading, IconButton, Text } from "@radix-ui/themes";
+import { decodeWinbaState, getWinbaStateAddress, getPoolBonusAddress, getPoolLpAddress } from "winba-core";
+import { useAccount, useWalletAddress } from "winba-react";
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { TokenAvatar } from "@/components";
+import { useTokenMeta } from "@/hooks/useTokenMeta";
+import { UiPool } from "../Dashboard/PoolList";
 
-import { TokenAvatar } from "@/components"
-import { SolanaAddress } from "@/components/SolanaAddress"
-import { useTokenMeta } from "@/hooks/useTokenMeta"
-import { UiPool } from "../Dashboard/PoolList"
-
-export function PoolHeader({pool}: {pool: UiPool}) {
-  const token = useTokenMeta(pool.underlyingTokenMint)
-  const gambaState = useAccount(getGambaStateAddress(), decodeGambaState)
-  const userPublicKey = useWalletAddress()
-  const navigate = useNavigate()
-  const isPoolAuthority = pool?.poolAuthority?.equals(userPublicKey)
-  const isGambaStateAuthority = gambaState?.authority?.equals(userPublicKey)
+export function PoolHeader({ pool }: { pool: UiPool }) {
+  const token = useTokenMeta(pool.underlyingTokenMint);
+  const winbaState = useAccount(getWinbaStateAddress(), decodeWinbaState);
+  const userPublicKey = useWalletAddress();
+  const navigate = useNavigate();
+  const isPoolAuthority = pool?.poolAuthority?.equals(userPublicKey);
+  const isWinbaStateAuthority = winbaState?.authority?.equals(userPublicKey);
 
   return (
     <Flex gap="4" align="center">
       <NavLink to={"/pool/" + pool.publicKey.toBase58()} style={{ display: "contents", color: "unset" }}>
-        <TokenAvatar
-          size="3"
-          mint={pool.underlyingTokenMint}
-        />
+        <TokenAvatar size="3" mint={pool.underlyingTokenMint} />
         <Flex align="center" gap="2">
-          <Heading>
-            {token.name}
-          </Heading>
+          <Heading>{token.name}</Heading>
           <Text color="gray" size="4">
             {token.symbol}
           </Text>
         </Flex>
       </NavLink>
-
-      {(isPoolAuthority || isGambaStateAuthority) && (
+      {(isPoolAuthority || isWinbaStateAuthority) && (
         <IconButton size="2" variant="ghost" onClick={() => navigate("/pool/" + pool.publicKey.toString() + "/configure")}>
           <GearIcon />
         </IconButton>
@@ -51,27 +43,27 @@ export function PoolHeader({pool}: {pool: UiPool}) {
           <Dialog.Description>
             <Flex direction="column">
               <Text color="gray" size="2">Token mint</Text>
-              <SolanaAddress address={pool.underlyingTokenMint} />
+              <Text>{pool.underlyingTokenMint.toString()}</Text>
             </Flex>
             <Flex direction="column">
               <Text color="gray" size="2">LP Token mint</Text>
-              <SolanaAddress address={getPoolLpAddress(pool.publicKey)} />
+              <Text>{getPoolLpAddress(pool.publicKey).toString()}</Text>
             </Flex>
             <Flex direction="column">
               <Text color="gray" size="2">Bonus Token mint</Text>
-              <SolanaAddress address={getPoolBonusAddress(pool.publicKey)} />
+              <Text>{getPoolBonusAddress(pool.publicKey).toString()}</Text>
             </Flex>
             <Flex direction="column">
               <Text color="gray" size="2">Pool Address</Text>
-              <SolanaAddress address={pool.publicKey} />
+              <Text>{pool.publicKey.toString()}</Text>
             </Flex>
             <Flex direction="column">
               <Text color="gray" size="2">Pool Authority</Text>
-              <SolanaAddress address={pool?.poolAuthority!} />
+              <Text>{pool?.poolAuthority?.toString()}</Text>
             </Flex>
           </Dialog.Description>
         </Dialog.Content>
       </Dialog.Root>
     </Flex>
-  )
+  );
 }
